0c1c8e4ab96aa349497ff01f0a10affe40947802
niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2025-12-02 02:18:43 -0800

    stampa stipendi

--- uni/file/binari/stipendi/stampa_stipendi.c
+++ uni/file/binari/stipendi/stampa_stipendi.c
@@ -2,25 +2,19 @@
 #include "dipendente.c"
 
 int main() {
-    Dipendente dipendenti[] = {
-        {"Rossi", 2500.50},
-        {"Bianchi", 3000.75},
-        {"Verdi", 2800.00},
-        {"Neri", 3200.25},
-        {"Russo", 2700.80}
-    };
+    Dipendente dipendenti[10];
 
-    FILE *f = fopen("stipendi.dat", "wb");
+    FILE *f = fopen("stipendi.dat", "rb");
 
     if(f == NULL) {
         perror("Errore nell'apertura del file");
         return -1;
     }
 
-    int num_dipendenti = sizeof(dipendenti) / sizeof(Dipendente);
 
-    for(int i = 0; i < num_dipendenti; i++) {
-        fwrite(&dipendenti[i], sizeof(Dipendente), 1, f);
+    while (fread(dipendenti, sizeof(Dipendente), 1, f) != 0) {
+        printf("Nome: %s, Stipendio: %.2f\n", dipendenti[0].nome, dipendenti[0].stipendio);
+    
     }
 
     fclose(f);
