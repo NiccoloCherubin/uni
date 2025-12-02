d2cf2ecff77ab04ffe7aae56bc5bf483f4c84ad6
niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2025-12-02 02:26:13 -0800

    aggiorna tutti gli stipendi

---
+++ uni/file/binari/stipendi/aggiorna_stipendi.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dipendente.c"
+
+int main(int argc, char ** argv)
+{
+    FILE *f = fopen("stipendi.dat", "r+b"); // apertura in lettura e scrittura
+
+    if (f == NULL) {
+        perror("Errore nell'apertura del file");
+        exit(-1);
+    }
+
+    Dipendente d;
+
+    while(fread(&d, sizeof(Dipendente),1, f) != 0)
+    {
+        d.stipendio *= 1.10; // aumento del 10%
+        fseek(f, -sizeof(Dipendente), SEEK_CUR); // torna indietro di una struttura
+        fwrite(&d, sizeof(Dipendente), 1, f); // riscrive la struttura aggiornata
+    }
+
+
+    return 0;
+}
