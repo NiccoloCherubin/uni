c9d3735eff32843cb7ee94551af9979ed6192d6e
niccolo <niccolo.cherubin@iisviolamarchesini.edu.it>
2025-12-11 01:39:36 -0800

    creazione definzione funzioni lista

--- uni/esercizi_parziale/linked/lista.h
+++ uni/esercizi_parziale/linked/lista.h
@@ -1,4 +1,11 @@
 #include "nodo.h";
 
 typedef Nodo *Lista;
-;
+
+void nuovaLista(Lista *l);
+void inserimentoTesta(Lista *l, Veicolo val);
+void inserimentoOrd(Lista *l,Veicolo val);
+void elimina_elemento(Lista *l, Veicolo val);
+void elimina_tutto(Lista *l);
+int lunghezza(Lista l);
+void stampa(Lista l);
